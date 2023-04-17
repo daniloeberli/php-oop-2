6098353f49d828cafc8ad8856eea3bc1cd384444
daniloeberli <danilo.eberli@gmail.com>
2023-04-17 16:40:12 +0200

    feat: Cibo class

---
+++ Models/Cibo.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/Product.php';
+
+class Cibo extends Product {
+
+    function __construct(/*public Category $category,*/ public string $name, public string $description, public float $price, public float $weigth, public string $bio){
+
+        parent::__construct(/*$category,*/ $name, $description, $price, $weigth, $bio);
+
+        $this->weigth = $weigth;
+        $this->bio = $bio;
+    }
+}
+
+//$cibo2 = new Cibo('crock1', 'description',23,10.5, 'biologico');
+
+//var_dump($cibo2);
+
+?>
