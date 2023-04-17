850113024c18fb90fc91bfdb4c66f930e0b2d78d
daniloeberli <danilo.eberli@gmail.com>
2023-04-17 17:00:41 +0200

    feat: Accessori class added

---
+++ Models/Accessori.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/Product.php';
+
+class Accessori extends Product{
+
+    function __construct(/*public Category $category,*/ public string $name, public string $description, public float $price, public string $materiale, public string $dimensioni){
+
+        parent::__construct(/*$category,*/ $name, $description, $price);
+
+        $this->materiale = $materiale;    
+        $this->dimensioni = $dimensioni;    
+        
+    }
+}
+
+$cuccia = new Accessori('cuccia super','description',30,'cotone','50x40');
+
+var_dump($cuccia);
+
+?>
